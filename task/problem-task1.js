1999219e3f38c4e44a9b94126dc480e8f7ed901e
nusratjahan7 <nusratjahan77222@gmail.com>
2026-01-31 12:21:49 +0600

    problem task1 added

---
+++ task/problem-task1.js
@@ -0,0 +1,13 @@
+//Find the lowest number in the array below.
+
+const heights2 = [167, 190, 120, 165, 137];
+
+let lowest = heights2[0];
+
+for (let i = 1; i < heights2.length; i++){
+    if (heights2[i] < lowest){
+        lowest = heights2[i];
+    }
+}
+
+console.log(lowest);
