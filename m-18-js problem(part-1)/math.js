b05e6743752c05db7cdf95b83458f10653890fac
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-02 22:01:03 +0600

    added math js

---
+++ m-18-js problem(part-1)/math.js
@@ -0,0 +1,22 @@
+
+const min = Math.min(45, 21, 65, 99, 126, 5)
+console.log(min);
+
+const max = Math.max(45, 21, 65, 99, 126, 5)
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(5-10)); // negative k positive kore dey
+console.log(Math.round(2.85)); // purno songkha kore
+console.log(Math.floor(2.95));
+console.log(Math.floor(2.9995)); // floor niche namay
+console.log(Math.ceil(2.9995));
+console.log(Math.ceil(2.0001)); // ceil upore uthay
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+console.log('----------------');
+
+const rand = Math.round(Math.random()*10);
+console.log(rand);
