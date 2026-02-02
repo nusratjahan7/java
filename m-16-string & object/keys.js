a5281c6f04c3d9a96c571189ce6703f2a16a0918
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-02 19:40:53 +0600

    change name

---
+++ m-16-string & object/keys.js
@@ -0,0 +1,9 @@
+const computer = {
+    brand: 'lenovo',
+    price: 35000,
+    processor: 'intel',
+    hdd: '512gb'
+}
+
+const keys = Object.keys(computer);
+console.log(keys)
---
+++ m-16-string & object/nested.js
@@ -0,0 +1,13 @@
+const college = {
+    name: 'vnc',
+    class: ['11', '12'],
+    events: ['science fair', 'bijoy dibos', '21 feb'],
+    unique: {
+        color: 'blue',
+        result: {
+            gpa: 5,
+            merit: 'top'
+        }
+    }
+}
+console.log(college.unique.result.merit);
