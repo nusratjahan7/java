81e5ad2a39dfb52ac82a5553543dc4d31b5d335a
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-02 12:20:13 +0600

    function task 1 added

---
+++ task/function-task1.js
@@ -0,0 +1,10 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+
+function multiplyFourNumbers(a, b, c, d){
+    let multiply = a * b * c * d;
+    return multiply;
+}
+
+let result = multiplyFourNumbers(2, 3, 4, 5);
+console.log(result);
