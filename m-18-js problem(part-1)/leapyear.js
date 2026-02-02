2450ab4f415cd35eb8b5cf2a9ae58c80b425f3d0
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-02 20:20:19 +0600

    added leapyear js

---
+++ m-18-js problem(part-1)/leapyear.js
@@ -0,0 +1,38 @@
+/*
+* Simple logic
+* Year will be a leap year if the year is divisible by 4
+*/
+
+function isLeapYear (year){
+    if (year % 4 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const leapYear = isLeapYear(2028);
+console.log(leapYear);
+
+
+/*
+ * 1. Those year that not divisible by 100 and if the year is divisible by 4: then it will be a leap year. 
+ * 2.
+*/
+
+function isLeapYear2(year){
+    if(year % 100 !== 0 && year % 4 === 0){
+        return true;
+    }
+    else if(year % 100 === 0 && year % 400 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+const isleap = isLeapYear2(2100);
+const isleap2 = isLeapYear2(2400);
+const isleap3 = isLeapYear2(2052);
+console.log(isleap, isleap2, isleap3);
