2d54e3125eaade290ffc47581fa14a2dba892b31
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-03 12:10:47 +0600

    solve js problem task 3

--- task/jsproblem-task3.js
+++ task/jsproblem-task3.js
@@ -1,1 +1,15 @@
 // Write a function to count the number of vowels in a string.
+
+function countVowels(str){
+    let count = 0;
+    let vowels = "aeiouAEIOU";
+    for (let i = 0; i < str.length; i++){
+        if (vowels.includes(str[i])){
+            count++;
+        }
+    }
+    return count;
+}
+
+const count = countVowels('Education');
+console.log(count);
