d2e2de91d31c124b0ef4b504f8155174f09ca701
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-03 19:24:15 +0600

    added max3 js

---
+++ m-19-js problem(part-2)/max3.js
@@ -0,0 +1,18 @@
+const jim = 56;
+const tim = 34;
+const kim = 89;
+
+if (jim > tim && jim > kim){
+    console.log('Jim is the ultimate boss');
+}
+else if (tim > jim && tim > kim){
+    console.log('Tim is the boss');
+}
+else {
+    console.log('Kim is the kardahians boss');
+}
+
+/*------------------------------*/
+
+const max = Math.max(12 , 45, 3, 554, 8, 67, 23, 56, 9);
+console.log('max isuing Math.max', max);
