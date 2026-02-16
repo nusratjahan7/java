d81bc0ad923408408d8afe2d389f21bba40a058e
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-16 12:36:49 +0600

    dom event

--- string & object/concat.js
+++
@@ -1,8 +0,0 @@
-const first = 'Abid';
-const last = 'Nabid';
-const fullName = first + ' ' + last;
-console.log(fullName);
-const fullName2 = first.concat(last);
-console.log(fullName2);
-
-console.log(last.includes('N'));
