84af13cfed56605dcf22d6f216eee84bc3361687
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-25 20:41:46 +0600

    basic es6

---
+++ Es6-m26/let-const.js
@@ -0,0 +1,18 @@
+const name = 'Bangladesh';
+const countryName = 'BD ' + name;
+console.log(countryName);
+
+let price = 120;
+price = 130;
+console.log(price);
+
+const dress = ['shirt', 'pant', 'shoe'];
+dress.push('belt');
+console.log(dress);
+
+const student = {
+    name: 'John',
+    age: 25
+}
+student.name = 'jone';
+console.log(student);
