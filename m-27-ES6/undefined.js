42bf5c5b20f8f087faf9ba5559f30be43f85761c
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-26 21:02:16 +0600

    undefined

---
+++ m-27-ES6/undefined.js
@@ -0,0 +1,20 @@
+let data;
+// console.log(data);
+
+const sum = (a, b) => {
+    // console.log(a, b);
+}
+sum(5);
+
+const student = {
+    name: 'Tormuj',
+    age: 22,
+    salary: null
+}
+delete student.name;
+// console.log(student.marks);
+// console.log(student.name);
+
+const arr = [1, 2, 4, 5];
+// console.log(arr[8]);
+console.log(typeof null);
