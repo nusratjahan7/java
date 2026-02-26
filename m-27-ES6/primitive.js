3e19e666c4462f3aade406b149eb376a8ffca4a4
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-26 20:43:09 +0600

    premitive

---
+++ m-27-ES6/primitive.js
@@ -0,0 +1,18 @@
+let data = 4;
+data = 'tormuj';
+data = false;
+data = {price: 22};
+data = [];
+// console.log(data);
+// console.log(typeof data);
+
+
+// primitive data type
+let a = 4;
+let b = 'Tomuj';
+let c = false;
+
+// non- primitive
+let d = {price: 22};
+let e = [1, 2, 3, 4];
+
