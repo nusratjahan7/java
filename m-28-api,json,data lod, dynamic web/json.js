639849eaab17d7d4aeeb10faca923e80f0cd9292
nusratjahan7 <nusratjahan77222@gmail.com>
2026-03-01 21:35:37 +0600

    json

---
+++ m-28-api,json,data lod, dynamic web/json.js
@@ -0,0 +1,19 @@
+const loadData = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then((Response) => Response.json())
+    .then((data) => console.log(data));
+}
+
+const loadPost = () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+        console.log(json);
+        displayPost(json);
+    });
+};
+
+const displayPost = (post) => {
+    console.log(post);
+}
---
+++ m-28-api,json,data lod, dynamic web/script.js
@@ -0,0 +1,29 @@
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then((Response) => Response.json())
+//     .then((data) => console.log(data));
+
+
+    
+
+// console.log("Explore API");
+
+// const person = {
+//     name: "selim",
+//     fruit: "dalim",
+//     dish: "halim",
+//     friends: ["alim", "kolim", "lamim"],
+//     isRich: false,
+//     money: 34000
+// }
+// console.log(person, typeof person);
+
+// // JSON --> JS object with Notation
+
+// // JSON.stringify
+// const personJSON = JSON.stringify(person);
+// console.log(personJSON, typeof personJSON);
+
+// // JSON.parse
+// const parseJSON = JSON.parse(personJSON);
+// console.log(parseJSON, typeof parseJSON);
